d24f20bd768e611ae9710955a57ee224dd8b1c31
wan <421436804@qq.com>
2016-08-19 16:27:46 +0800

    init message

--- app/src/main/java/helloworld/wan/com/helloworld/MainActivity.java
+++ app/src/main/java/helloworld/wan/com/helloworld/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import helloworld.wan.com.eshorelibrary.Tools;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("wan","------------");
+        Log.i("wan","------------"+ Tools.getHellow());
     }
 }
---
+++ eshorelibrary/src/main/java/helloworld/wan/com/eshorelibrary/Tools.java
@@ -0,0 +1,10 @@
+package helloworld.wan.com.eshorelibrary;
+
+/**
+ * Created by admin on 2016/8/19.
+ */
+public class Tools {
+    public static String getHellow() {
+        return "hello world";
+    }
+}
